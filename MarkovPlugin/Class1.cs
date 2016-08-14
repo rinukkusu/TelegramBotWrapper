3172d0cf0f2a52db8585ca637da43e91dc2b9652
Max Riegler <rinukkusu@sub-r.de>
2016-08-14 11:52:56 +0200

    added base for markov plugin

---
+++ MarkovPlugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovPlugin
+{
+    public class Class1
+    {
+    }
+}
